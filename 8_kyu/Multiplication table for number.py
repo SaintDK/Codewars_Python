09677724074753c4b3ae4984992adae34ba926b9
DK <ivan.yakimishin.00@mail.ru>
2024-11-26 17:05:37 +0300

    commit

---
+++ 8_kyu/Multiplication table for number.py
@@ -0,0 +1,14 @@
+def multi_table(number):
+    number1 = number * 1
+    number2 = number * 2
+    number3 = number * 3
+    number4 = number * 4
+    number5 = number * 5
+    number6 = number * 6
+    number7 = number * 7
+    number8 = number * 8
+    number9 = number * 9
+    number10 = number * 10
+    return f'1 * {number} = {number1}\n2 * {number} = {number2}\n3 * {number} = {number3}\n4 * {number} = {number4}\n5 * {number} = {number5}\n6 * {number} = {number6}\n7 * {number} = {number7}\n8 * {number} = {number8}\n9 * {number} = {number9}\n10 * {number} = {number10}'
+
+multi_table(5)
