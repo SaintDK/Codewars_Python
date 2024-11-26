50fbfd064c947466d72f767f17e97775d7c5a921
DK <ivan.yakimishin.00@mail.ru>
2024-11-26 15:37:58 +0300

    commit

--- 8_kyu/Filter out the geese.py
+++ 8_kyu/Filter out the geese.py
@@ -8,3 +8,21 @@ def goose_filter(birds):
             birds.remove(geese[repeat])
 
     return birds
+def goose_filter(birds):
+
+    repeat = len(geese)
+    while repeat:
+        repeat = repeat - 1
+        if geese[repeat] in birds:
+            birds.remove(geese[repeat])
+
+    return birds
+def goose_filter(birds):
+
+    repeat = len(geese)
+    while repeat:
+        repeat = repeat - 1
+        if geese[repeat] in birds:
+            birds.remove(geese[repeat])
+
+    return birds
